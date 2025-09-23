1edf6b321ef86f2dc710696e80475520c0854769
DylanJA0809 <dylanjimenezalfaro.ml@gmail.com>
2025-09-23 05:14:08 -0600

    Workshop_1 paginas simples para mostrar diferencias entre HTML y PHP

---
+++ Workshop_1/fechayhora.php
@@ -0,0 +1,12 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fecha y Hora - PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Fecha y hora actual: " . date("d/m/Y H:i:s") . "</h1>";
+    ?>
+</body>
+</html>
